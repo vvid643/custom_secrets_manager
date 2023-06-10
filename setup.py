fa9fa60a6a112a19d2a0c97da15992f0c886ab0e
Vashishtha <vidyarthi.v5@gmail.com>
2023-06-10 20:25:10 +0530

    Revise stable version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     packages=find_packages(),
     install_requires=REQUIRES,
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
